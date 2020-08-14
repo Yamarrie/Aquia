20d8e3f9a32480a25e7800ee14610c98368e4a95
Yamarrie <69699350+Yamarrie@users.noreply.github.com>
2020-08-14 23:30:45 +0100

    Create main.py
    Pasted in all the code I've already worked on.

---
+++ main.py
@@ -0,0 +1,146 @@
+import time as t
+import os
+
+# Finds out what batshit crazy folder the program is contained in
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+
+
+def main():
+
+    def commandprompt():
+        print("Command Prompt V1")
+        cmd = input(": ")
+        if cmd ==
+
+
+    # Notepad but with more options dickwad
+    def improvedNotepad():
+        print("--------------")
+        print("Better Notepad")
+        print("--------------")
+        print()
+        holder1 = input("Enter file name (Including file extension): ")
+        print()
+        print("Options: Append ")
+        print("         Create")
+        input2 = input("Enter what you would like to do: ")
+        if input2 == "append":
+            holder2 = "a"
+        if input2 == "Append":
+            holder2 = "a"
+        if input2 == "create":
+            holder2 = "w+"
+        if input2 == "Create":
+            holder2 = "w+"
+        f = open(holder1, holder2)
+        input1 = input(": ")
+        f.write(input1)
+        f.close()
+
+        mainMenu()
+
+    # Why is this shit not fucking working? Am I retarded? Like medically?
+    def fileReader():
+        print("------------------")
+        print(".txt File Reader")
+        print("------------------")
+        openfilename1 = input("Enter file name: ")
+        openfilename2 = os.path.join(THIS_FOLDER, openfilename1)
+        switch1 = os.path.isfile(openfilename2)
+        if switch1 == True:
+            f = open(openfilename2, "r")
+            contents1 = f.read()
+            print()
+            print(contents1)
+            t.sleep(2)
+            print()
+            input("Press enter to continue...")
+            print()
+            mainMenu()
+        if switch1 == False:
+            print()
+            print("File does not exist")
+            print()
+            input("Press enter to continue...")
+            print()
+            mainMenu()
+        mainMenu()
+
+    # Shit was worked on and now works, smartass
+    def notepad():
+        print("---------")
+        print("Notepad")
+        print("---------")
+        filename1 = str(input("Enter file name: "))
+        f = open(filename1 + ".txt", "w+")
+        padcontents = str(input(": "))
+        f.write(padcontents)
+        f.close()
+
+        mainMenu()
+
+    # This shit is basically just a calculator
+    def calculator():
+        print("-----------")
+        print("Calculator")
+        print("-----------")
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        op1 = str(input("Enter the operation: "))
+
+        if op1 == "+":
+            ans1 = (num1 + num2)
+            print(ans1)
+        if op1 == "-":
+            ans1 = (num1 - num2)
+            print(ans1)
+        if op1 == "*":
+            ans1 = (num1 * num2)
+            print(ans1)
+        if op1 == "/":
+            ans1 = (num1 / num2)
+            print(ans1)
+
+        mainMenu()
+
+    # Main Menu shit, needs to be continuously updated
+    def mainMenu():
+        print("-----------")
+        print("Main Menu:")
+        print("----------")
+        print("1. Calculator")
+        print("2. Notepad")
+        print("3. File reader")
+
+        option1 = input(": ")
+
+        if option1 == "calc":
+            calculator()
+        elif option1 == "Calculator":
+            calculator()
+        elif option1 == "calculator":
+            calculator()
+        elif option1 == "!1":
+            calculator()
+        elif option1 == "!2":
+            notepad()
+        elif option1 == "notepad":
+            notepad()
+        elif option1 == "Notepad":
+            notepad()
+        elif option1 == "!3":
+            fileReader()
+        elif option1 == "File reader":
+            fileReader()
+        if option1 == "file reader":
+            fileReader()
+        if option1 == "listdir":
+            files = os.listdir()
+            print(files)
+        if option1 == "!4":
+            improvedNotepad()
+
+    mainMenu()
+
+
+main()
